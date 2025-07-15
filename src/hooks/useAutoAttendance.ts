ffd7615c8eef0dc79047708af3b014fb9cbbf41f
goblin225 <issaclevi225@gmail.com>
2025-07-15 16:26:51 +0530

    disabled attandance

--- src/hooks/useAutoAttendance.ts
+++ src/hooks/useAutoAttendance.ts
@@ -1,36 +1,36 @@
-import { useMutation } from '@tanstack/react-query';
-import { useEffect } from 'react';
-import { markAttendance } from '../services/service';
+// import { useMutation } from '@tanstack/react-query';
+// import { useEffect } from 'react';
+// import { markAttendance } from '../services/service';
 
-const useAutoAttendance = () => {
-  const user = JSON.parse(sessionStorage.getItem('user') || '{}');
-  const userId = user?.id || '';
+// const useAutoAttendance = () => {
+//   const user = JSON.parse(sessionStorage.getItem('user') || '{}');
+//   const userId = user?.id || '';
 
-  const mutation = useMutation({
-    mutationFn: markAttendance,
-    onSuccess: (data) => {
-      localStorage.setItem('attendance-marked', new Date().toDateString());
-    },
-    onError: (err) => {
-      console.error('Attendance marking failed:', err);
-    }
-  });
+//   const mutation = useMutation({
+//     mutationFn: markAttendance,
+//     onSuccess: (data) => {
+//       localStorage.setItem('attendance-marked', new Date().toDateString());
+//     },
+//     onError: (err) => {
+//       console.error('Attendance marking failed:', err);
+//     }
+//   });
 
-  useEffect(() => {
-    const now = new Date();
-    const hour = now.getHours();
+//   useEffect(() => {
+//     const now = new Date();
+//     const hour = now.getHours();
 
-    const todayKey = new Date().toDateString();
-    const markedDate = localStorage.getItem('attendance-marked');
+//     const todayKey = new Date().toDateString();
+//     const markedDate = localStorage.getItem('attendance-marked');
 
-    const alreadyMarkedToday = markedDate === todayKey;
+//     const alreadyMarkedToday = markedDate === todayKey;
 
-    if (hour >= 10 && hour < 11 && !alreadyMarkedToday) {
-      mutation.mutate(userId);
-    }
-  }, [mutation, userId]);
+//     if (hour >= 10 && hour < 11 && !alreadyMarkedToday) {
+//       mutation.mutate(userId);
+//     }
+//   }, [mutation, userId]);
 
-  return mutation;
-};
+//   return mutation;
+// };
 
-export default useAutoAttendance;
+// export default useAutoAttendance;
